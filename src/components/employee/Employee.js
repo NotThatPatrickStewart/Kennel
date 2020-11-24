0b417be09349004f7a30fa0b0fc13ab4cf818a88
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-24 11:41:06 -0600

    refactored code to interpolate dynamic name and kennel

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./Employee.css"
 
-export const Employee = () => ( //single line function with implied return
+export const Employee = ({ employee, }) => ( //single line function with implied return
     <section className="employee">
-        <h3 className="employee__name">Emma Beaton</h3>
-        <div className="employee__kennel">Nashville Kennels North</div>
+        <h3 className="employee__name">{employee.name}</h3>
+        <div className="employee__kennel">{employee.locationId}</div>
     </section>
 )
