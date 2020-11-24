193089d4a0f64bec72aa5a946d29a942112263bc
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-24 11:51:01 -0600

    refactored code to interpolate dynamic name and address

--- src/components/customer/Customer.js
+++ src/components/customer/Customer.js
@@ -1,9 +1,9 @@
 import React from "react"
 import "./Customer.css"
 
-export const Customer = () => ( //single line function with implied return
+export const Customer = ({ customer }) => ( //single line function with implied return
     <section className="customer">
-        <h3 className="customer__name">Emma Beaton</h3>
-        <div className="customer__address">Address: 100 Infinity Way</div>
+        <h3 className="customer__name">{customer.name}</h3>
+        <div className="customer__address">{customer.address}</div>
     </section>
 )
