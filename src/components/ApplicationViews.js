b89be4a1d596386a5067095384b48bc76379df08
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-28 14:34:30 -0600

    refactored AnimalList to get data from Location and Customer Providers

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,26 +1,31 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import { LocationProvider } from "./location/LocationProvider"
-import { AnimalProvider } from "./animal/AnimalProvider"
-import { LocationList } from "./location/LocationList"
-import { AnimalList } from "./animal/AnimalList"
+import React from "react";
+import { Route } from "react-router-dom";
+import { LocationProvider } from "./location/LocationProvider";
+import { AnimalProvider } from "./animal/AnimalProvider";
+import { LocationList } from "./location/LocationList";
+import { AnimalList } from "./animal/AnimalList";
+import { CustomerProvider } from "./customer/CustomerProvider";
 
 export const ApplicationViews = (props) => {
-    return (
-        <>
-            <LocationProvider>
-                {/* Render the location list when http://localhost:3000/ */}
-                <Route exact path="/">
-                    <LocationList />
-                </Route>
-            </LocationProvider>
+  return (
+    <>
+      <LocationProvider>
+        {/* Render the location list when http://localhost:3000/ */}
+        <Route exact path="/">
+          <LocationList />
+        </Route>
+      </LocationProvider>
 
-            <AnimalProvider>
-                {/* Render the animal list when http://localhost:3000/animals */}
-                <Route path="/animals">
-                    <AnimalList />
-                </Route>
-            </AnimalProvider>
-        </>
-    )
-}
+          <AnimalProvider>
+      <LocationProvider>
+        <CustomerProvider>
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <Route path="/animals">
+              <AnimalList {...props}/>
+            </Route>
+        </CustomerProvider>
+      </LocationProvider>
+          </AnimalProvider>
+    </>
+  );
+};
