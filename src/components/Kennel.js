4cf86fe908ba4b3209e6542d637c6db1f2974bb5
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-30 11:26:34 -0600

    changed all code in Kennel.js with code from chapter 9

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,28 +1,27 @@
 import React from "react"
-import { LocationList } from "./location/LocationList" 
-import { LocationProvider } from "./location/LocationProvider" 
-import { location } from "./location/Location"
-import "./Kennel.css"
-import { Animal } from "./animal/Animal"
-import { AnimalList } from "./animal/AnimalList" 
-import { AnimalProvider } from "./animal/AnimalProvider"
-import { EmployeeProvider } from "./employee/EmployeeProvider"
-import { EmployeeList } from "./employee/EmployeeList"
-import { Employee } from "./employee/Employee"
-import { CustomerProvider } from "./customer/CustomerProvider"
-import { CustomerList } from "./customer/CustomerList"
-import { Customer } from "./customer/Customer"
-import { NavBar } from "./nav/NavBar"
+import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+import "./Kennel.css"
 
+export const Kennel = () => (
+    <>
+        <Route render={() => {
+            if (localStorage.getItem("kennel_customer")) {
+                return (
+                    <>
+                        <Route render={props => <NavBar {...props} />} />
+                        <Route render={props => <ApplicationViews {...props} />} />
+                    </>
+                )
+            } else {
+                return <Redirect to="/login" />
+            }
+        }} />
 
-export const Kennel = () => ( //single line function with implied return
-
-//use empty angle brackets as a nameless container
-//because this function in JSX needs to return a single parent,
-//it cannot have siblings (as this would without the brackets)
-    <> 
-        <NavBar />        
-        <ApplicationViews />
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <Register {...props} />} />
     </>
 )
