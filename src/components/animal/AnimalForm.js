a3a7898402858edd458c211ad24e6a8ba318509d
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-30 14:22:19 -0600

    finished adding AnimalForm function with useEffect for getLocations and returned fields for adding animal name and location, added save button. commented out all customer code that is no longer being utilized.

--- src/components/animal/AnimalForm.js
+++ src/components/animal/AnimalForm.js
@@ -1,36 +1,75 @@
 import React, { useContext, useRef, useEffect } from "react"
-import { CustomerContext } from "../customer/CustomerProvider"
+// import { CustomerContext } from "../customer/CustomerProvider"
+// import { EmployeeContext } from "../employee/EmployeeProvider"
+import { LocationContext } from "../location/LocationProvider"
 import { AnimalContext } from "./AnimalProvider"
 
 export const AnimalForm = (props) => {
     const { addAnimal } = useContext(AnimalContext)
-    const {customers, getCustomers} = useContext(CustomerContext)
+    // const {customers, getCustomers} = useContext(CustomerContext)
     const { locations, getLocations } = useContext(LocationContext)
-    const { employees, getEmployees } = useContext(EmployeeContext)
+    // const { employees, getEmployees } = useContext(EmployeeContext)
 
     const name = useRef(null)
-    const customer = useRef(null)
     const location = useRef(null)
-    const employee = useRef(null)
+    // const customer = useRef(null)
+    // const employee = useRef(null)
+
+
 
     useEffect(() => {
-        getEmployees()
-        .then(getLocations)
-        .then(getCustomers)
+        getLocations()
     }, [])
 
     const constructNewAnimal = () => {
-        const customerId = parseInt(customer.current.value)
+        // const customerId = parseInt(customer.current.value)
         const locationId = parseInt(location.current.value)
-        const employeeId = parseInt(employee.current.value)
+        // const employeeId = parseInt(employee.current.value)
 
-        if (customerId === 0 || locationId === 0 || employeeId === 0) {
-            window.alert("Please make sure all fields are selected")
+        if (locationId === 0) {
+            window.alert("Please make sure location is selected")
         }
         else {
             addAnimal({
-                
+                name: name.current.value,
+                locationId,
+                customerId: parseInt(localStorage.getItem("kennel_customer"))
+
             })
+            .then(() => props.history.push("/animals"))
         }
     }
+    return (
+        <form className="animalForm">
+            <h2 className="animalForm__title">New Animal</h2>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="animalName">Animal name: </label>
+                    <input type="text" id="animalName" ref={name} required autoFocus className="form-control" placeholder="Animal name" />
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="location">Assign to location: </label>
+                    <select defaultValue="" name="location" ref={location} id="animalLocation" className="form-control" >
+                        <option value="0">Select a location</option>
+                        {locations.map(a => (
+                            <option key={a.id} value={a.id}>
+                                {a.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+            </fieldset>
+            
+            <button type="submit"
+                onClick={evt => {
+                    evt.preventDefault() // Prevent browser from submitting the form
+                    constructNewAnimal()
+                }}
+                className="btn btn-primary">
+                Save Appointment
+            </button>
+        </form>
+    )
 }
