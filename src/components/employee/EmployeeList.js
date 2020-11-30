7b02e15abfa095c7246f8d748d6dc930c0b3dd6b
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-30 15:35:54 -0600

    refactored return statement fro dynamic pathway

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,54 +1,56 @@
-import React, { useContext, useEffect } from "react"
-import { EmployeeContext } from "./EmployeeProvider"
-import { Employee } from "./Employee"
-import "./Employee.css"
-import { LocationContext } from "../location/LocationProvider"
-import { AnimalContext } from "../animal/AnimalProvider"
-
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { EmployeeContext } from "./EmployeeProvider";
+// import { Employee } from "./Employee";
+import "./Employee.css";
+// import { LocationContext } from "../location/LocationProvider"
+// import { AnimalContext } from "../animal/AnimalProvider"
 
 export const EmployeeList = (props) => {
-    // This state changes when `getEmployees()` is invoked below
-    const { employees, getEmployees } = useContext(EmployeeContext)
-    const { animals, getAnimals } = useContext(AnimalContext)
-    const { locations, getLocations } = useContext(LocationContext)
+  // This state changes when `getEmployees()` is invoked below
+  const { employees, getEmployees } = useContext(EmployeeContext);
+  // const { animals, getAnimals } = useContext(AnimalContext)
+  // const { locations, getLocations } = useContext(LocationContext)
 
-
-    /*
+  /*
         What's the effect this is reponding to? Component was
         "mounted" to the DOM. React renders blank HTML first,
         then gets the data, then re-renders.
     */
-    useEffect(() => {
-        console.log("EmployeeList: Initial render before data")
-        getAnimals()
-        .then(getLocations)
-        .then(getEmployees)
-    }, [])
+  useEffect(() => {
+    // console.log("EmployeeList: Initial render before data")
+    getEmployees();
+    // .then(getLocations)
+  }, []);
 
-    /*
+  /*
         This effect is solely for learning purposes. The effect
         it is responding to is that the employee state changed.
     */
-    // useEffect(() => {
-    //     console.log("EmployeeList: Employee state changed")
-    //     console.log(employees)
-    // }, [employees])
+  // useEffect(() => {
+  //     console.log("EmployeeList: Employee state changed")
+  //     console.log(employees)
+  // }, [employees])
 
-    return (
-        <div className="employees">
-            <h1>Employees</h1>
-            <button onClick={() => props.history.push("/employees/create")}>
-                Add Employee
-            </button>
+  return (
+    <div className="employees">
+      <h1>Employees</h1>
+      <button onClick={() => props.history.push("/employees/create")}>
+        Add Employee
+      </button>
 
-        {console.log(employees, animals, locations)}
-        {
-                employees.map(employee => {
-                    const clinic = locations.find(l => l.id === employee.locationId)
-                    const pet = animals.find(a => a.id === employee.animalId)
-                return <Employee key={employee.id} employee={employee} location={clinic} animal={pet} />
-                })
-        }
-        </div>
-    )
-}
+      <article className="employeeList">
+        {/* {console.log(employees, animals, locations)} */}
+        {employees.map((employee) => {
+          //   const clinic = locations.find((l) => l.id === employee.locationId);
+          //   const pet = animals.find((a) => a.id === employee.animalId);
+          return (
+            <Link key={employee.id} to={`/employees/${employee.id}`}>
+              <h3>{employee.name}</h3>
+            </Link>
+          );
+        })}
+      </article>
+    </div>
+  );
+};
