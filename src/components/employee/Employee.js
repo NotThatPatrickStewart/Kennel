131d8fc965874bbaa1fd055d5007927857debe12
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-30 11:19:47 -0600

    refactored to include location and animal parameters

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -1,9 +1,10 @@
 import React from "react"
 import "./Employee.css"
 
-export const Employee = ({ employee, }) => ( //single line function with implied return
+export const Employee = ({ employee, location, animal }) => ( //single line function with implied return
     <section className="employee">
         <h3 className="employee__name">{employee.name}</h3>
-        <div className="employee__kennel">{employee.locationId}</div>
+        <div className="employee__kennel">Works at: {location.name}</div>
+        <div className="employee__animal">Cares for: {animal.name}</div>
     </section>
 )
