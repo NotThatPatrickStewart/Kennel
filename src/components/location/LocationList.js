240cb958009855110dae8eb7a7caaaa62f65fdb2
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-01 11:45:29 -0600

    refactored LocationList function to include employees and animals, refactored return function to addd hyperlinks and indivusal card info, code taken from chap 12

--- src/components/location/LocationList.js
+++ src/components/location/LocationList.js
@@ -1,11 +1,17 @@
 import React, { useContext, useEffect } from "react"
 import { LocationContext } from "./LocationProvider"
-import { Location } from "./Location"
+import { EmployeeContext } from "../employee/EmployeeProvider"
+import { AnimalContext } from "../animal/AnimalProvider"
+// import { Location } from "./Location"
+import { Link } from "react-router-dom"
 import "./Location.css"
 
-export const LocationList = () => {
+
+export const LocationList = (props) => {
     // This state changes when `getLocations()` is invoked below
     const { locations, getLocations } = useContext(LocationContext)
+    const { employees, getEmployees } = useContext(EmployeeContext)
+    const {animals, getAnimals } = useContext(AnimalContext)
 
     /*
         What's the effect this is reponding to? Component was
@@ -13,23 +19,44 @@ export const LocationList = () => {
         then gets the data, then re-renders.
     */
     useEffect(() => {
-        console.log("LocationList: Initial render before data")
-        getLocations()
+        // console.log("LocationList: Initial render before data")
+        getLocations().then(getEmployees).then(getAnimals)
     }, [])
 
     /*
         This effect is solely for learning purposes. The effect
         it is responding to is that the location state changed.
     */
-    useEffect(() => {
-        console.log("LocationList: Location state changed")
-        console.log(locations)
-    }, [locations])
+    // useEffect(() => {
+    //     console.log("LocationList: Location state changed")
+    //     console.log(locations)
+    // }, [locations])
 
     return (
         <div className="locations">
         {
-            locations.map(loc => <Location key={loc.id} location={loc} />)
+            locations.map(location => {
+                location.employees = employees.filter(e => e.locationId === location.id)
+                location.animals = animals.filter(a => a.locationId === location.id)
+
+                return <article key={`location--${location.id}`} className="card location" style={{ width: `18red` }}>
+                    <section className="card-body">
+                        <Link className="card-link"
+                            to={{
+                                pathname: `/locations/${location.id}`,
+                                state: { chosenLocation: location }
+                            }}>
+                                <h2 className="card-title"> {location.name} </h2>
+                            </Link>
+                    </section>
+                    <section>
+                        {`${location.employees.length} ${location.employees.length === 1 ? "employee" : "employees"}`}
+                    </section>
+                    <section>
+                        {`${location.animals.length} ${location.animals.length === 1 ? "animal" : "animals"}`}
+                    </section>
+                </article>
+            })
         }
         </div>
     )
