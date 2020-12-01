22f1c79f8b31962e8a4f51b6c757acb5b5d361dd
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-12-01 11:44:19 -0600

    added employee and animal parameters and interpolated amployee and animal name data

--- src/components/location/Location.js
+++ src/components/location/Location.js
@@ -1,9 +1,11 @@
 import React from "react"
 import "./Location.css"
 
-export const Location = ({ location }) => ( //single line function with implied return
+export const Location = ({ location, employee, animal }) => ( //single line function with implied return
     <section className="location">
         <h3 className="location__name">{location.name}</h3>
         <div className="location__address">{location.address}</div>
+        <div className="location__employee">{employee.name}</div>
+        <div className="location__animal">{animal.name}</div>
     </section>
 )
